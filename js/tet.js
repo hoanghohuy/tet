e77354f6e2fc375dc2059b3960efbb28b19bd7b5
hhhoang.h3@gmail.com <hhhoang.h3@gmail.com>
2023-12-12 15:54:45 +0700

    first commit

---
+++ js/tet.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $(".tet-thanks-slider .owl-carousel").owlCarousel({
+        dot: false,
+        items:1,
+        border: 8,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:1000,
+        loop: true,
+        autoWidth:true,
+    })
+
+    $('.tet-container .tet-contact .tet-form-submit').on('click', function() {
+        Swal.fire({
+    title: 'Cám ơn quý khách đã quan tâm tới dịch vụ của vietnam booking',
+  text: 'Thông tin đã được ghi nhận. Chúng tôi sẽ liên hệ lại Quý khách trong thời gian sớm nhất.',
+  confirmButtonText: 'Xác nhận'
+})
+    })
+  })
