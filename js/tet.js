7a27a4c0ecd5c52e1f94f51bfc928d86cfe22713
ho huy hoang <hohoang@vietnambooking.corp>
2023-12-15 09:57:43 +0700

    test

--- js/tet.js
+++ js/tet.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     $(".tet-thanks-slider .owl-carousel").owlCarousel({
         dot: false,
         items:1,
@@ -15,7 +14,13 @@ $(document).ready(function(){
         Swal.fire({
     title: 'Cám ơn quý khách đã quan tâm tới dịch vụ của vietnam booking',
   text: 'Thông tin đã được ghi nhận. Chúng tôi sẽ liên hệ lại Quý khách trong thời gian sớm nhất.',
-  confirmButtonText: 'Xác nhận'
+  confirmButtonText: '<img class="tet-popup-xacnhan-btn" src="/images/popup_xacnhan.png" />',
+  customClass: {
+    confirmButton: "tet-custom-confirm",
+    title: 'tet-custom-popup-title',
+    htmlContainer: 'tet-custom-popup-text',
+    popup: 'tet-custom-popup-container',
+  }
 })
     })
   })
